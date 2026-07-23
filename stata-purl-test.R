4230ef50aeea2d85f8bbfdcd85cbb22e91d4954e
Tom Palmer <remlapmot@hotmail.com>
2026-07-23 08:06:33 +0100

    Update stata-purl-test.R

--- stata-purl-test.R
+++ stata-purl-test.R
@@ -1,5 +1,7 @@
 Statamarkdown::purl_stata("stata-fig-test.Rmd", documentation = 2L)
 
+Statamarkdown::purl_stata("stata-fig-test-png.Rmd", documentation = 2L)
+
 Statamarkdown::purl_stata("stata-fig-test-qmd.qmd", documentation = 2L)
 
 Statamarkdown::purl_stata("stata-fig-test-qmd-dash.qmd", documentation = 2L)
